e142d203043d0f66fe6352c7dc25c5e9b4791568
Aidin Ghorbani <aidinghorbani89@gmail.com>
2025-05-09 11:11:13 +0330

    My first practice in python

---
+++ aidin.py
@@ -0,0 +1,26 @@
+t = 2
+u = 3
+print("area" , t * u, sep = ":")
+
+c = 9
+print("F" , c * 1.8 + 32 , sep = ":")
+
+g = 3
+print("mohit" , g * 2 * 3.14 , sep = ":")
+print("area" , g * g * 3.14 , sep = ":")
+
+x = 7
+y = 9
+z = 3
+print((x + y + z) // 3)
+
+e = 5
+f = 3
+print("area" , e * f / 2 , sep = ":")
+
+r = 15
+print(100 - r + 1404)
+
+o = 5
+p = 4
+print(o + p , o - p , o * p , o ** p , sep = ",")
